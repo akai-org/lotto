5cfd222c425f718fbe066720e49c30d5db611171
Rafał Rudol <rrrrudol@gmail.com>
2018-11-24 13:43:19 +0100

    add links

--- client/src/pages/AchievementsScreen.js
+++ client/src/pages/AchievementsScreen.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AchievementsScreen() {
   return (
     <div>
-      acziwments skrin
+      <Link to="/">Wróć na stronę główną!</Link>
     </div>
-  )
+  );
 }
--- client/src/pages/LoginScreen.js
+++ client/src/pages/LoginScreen.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginScreen() {
   return (
     <div>
-      login skrin
+      <Link to="/">Wróć na stronę główną!</Link>
     </div>
-  )
+  );
 }
