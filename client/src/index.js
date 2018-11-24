dffb039ab0f0f751991e9b819d4f14252581898e
Poe <piotr.a.ptak@icloud.com>
2018-11-24 13:17:14 +0100

    Initialize React Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { Switch } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import HomeScreen from './pages/HomeScreen';
+import LoginScreen from './pages/LoginScreen';
+import SettingsScreen from './pages/SettingsScreen';
+import AchievementsScreen from './pages/AchievementsScreen';
+import GameScreen from './pages/GameScreen';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Fragment>
+     <Router>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/settings" component={SettingsScreen} />
+            <Route path="/achievements" component={AchievementsScreen} />
+            <Route path="/game" component={GameScreen} />
+          </Switch>
+        </Router>
+  </Fragment>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
