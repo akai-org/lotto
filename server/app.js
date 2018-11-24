1039bfc10d5867fc4b22ffa4ae7753b9c308fcd9
Rafał Rudol <rrrrudol@gmail.com>
2018-11-24 16:20:31 +0100

    another fix

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const connect = require("./services/mongodb");
 const app = express();
 const port = 3001;
+const path = require("path");
 
 app.use(express.static(path.join(__dirname, "build")));
 
