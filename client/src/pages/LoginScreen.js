861aa79d475fdd3fcb6f7010729e8c29306948e4
Poe <piotr.a.ptak@icloud.com>
2018-11-24 18:40:06 +0100

    create useCookie hook

--- client/src/pages/LoginScreen.js
+++ client/src/pages/LoginScreen.js
@@ -1,93 +1,60 @@
-import React, { Fragment, Component, useState } from 'react';
-import { Link } from 'react-router-dom';
+// import Cookies from 'universal-cookie';
+import React, { Fragment } from 'react';
 import Layout from '../components/Layout';
 import Logo from '../components/Logo';
 import Button from '../components/Button';
-
-// import Form from '../components/Form';
-// import Input from '../components/Input';
 import Particles from 'react-particles-js';
-
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 
-export default class LoginScreen extends Component {
-  responseFacebook(response) {
-    const {
-      accessToken,
-      data_access_expiration_time,
-      email,
-      expiresIn,
-      name,
-      picture,
-      reauthorize_required_in,
-      signedRequest,
-      userID
-    } = response;
-    // const { email, password } = this.state;
-    // // const { onLogin, hist ory } = this.props;
+import useCookie from '../hooks/useCookie';
 
-    // try {
-    //   const { token, userName } = await request('auth/login', {
-    //     method: 'POST',
-    //     body: { email, password },
-    //   });
+// const cookies = new Cookies();
 
-    //   saveCookie(TOKEN_COOKIE, token);
-    //   saveCookie(USER_NAME_COOKIE, userName);
-    //   onLogin(userName);
-    //   history.push('/home');
-    // } catch (error) {
-    //   this.setState({
-    //     blocked: false,
-    //     email: '',
-    //     password: '',
-    //     errorMessage: 'Failed to login.',
-    //   });
-    // }
-  }
+export default function LoginScreen() {
+  const [token, setToken] = useCookie('token', '');
 
-  render() {
-    return (
-      <Fragment>
-        <Particles
-          params={{
-            particles: {
-              line_linked: {
-                shadow: {
-                  enable: true,
-                  color: '#3CA9D1',
-                  blur: 10
-                }
+  return (
+    <Fragment>
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: '#3CA9D1',
+                blur: 10
               }
             }
+          }
+        }}
+        style={{
+          width: '100%',
+          backgroundColor: `rgb(27, 43, 73)`,
+          position: 'absolute',
+          zIndex: '-1'
+        }}
+      />
+      <Layout distributed spanned narrow>
+        <Logo welcome title="Lootto" subtitle="get rekt." />
+        <FacebookLogin
+          appId="1088597931155576"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={response => {
+            setToken(response.accessToken);
           }}
-          style={{
-            width: '100%',
-            backgroundColor: `rgb(27, 43, 73)`,
-            position: 'absolute',
-            zIndex: '-1'
-          }}
+          render={renderProps => (
+            <Button
+              as="input"
+              type="submit"
+              primary
+              onClick={renderProps.onClick}
+            >
+              Zaloguj się przez facebook
+            </Button>
+          )}
         />
-        <Layout distributed spanned narrow>
-          <Logo welcome title="Lootto" subtitle="get rekt." />
-          <FacebookLogin
-            appId="1088597931155576"
-            autoLoad={true}
-            fields="name,email,picture"
-            callback={this.responseFacebook}
-            render={renderProps => (
-              <Button
-                as="input"
-                type="submit"
-                primary
-                onClick={renderProps.onClick}
-              >
-                Zaloguj się przez facebook
-              </Button>
-            )}
-          />
-        </Layout>
-      </Fragment>
-    );
-  }
+      </Layout>
+    </Fragment>
+  );
 }
