b417ef1a7f6c115eeaa12c0f7a41b21225418ea8
Rafał Rudol <rrrrudol@gmail.com>
2018-11-24 13:54:51 +0100

    add pm2 ecosystem

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
-require('dotenv').config();
-const express = require('express');
-const connect = require('./services/mongodb');
+require("dotenv").config();
+const express = require("express");
+const connect = require("./services/mongodb");
 const app = express();
 const port = 3001;
 
-app.get('/abc', (req, res) => res.send('Hello World!'));
+app.get("/user", (req, res) =>
+  res.send(JSON.stringify({ userId: "asdsadsa", firstName: "Stefan" }))
+);
 
-connect().then((db) => {
-  console.log('connected to mongodb');
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-}).catch((err) => {
-  console.error('mongodb connection failed', err);
-});
+app.get("/abc", (req, res) => res.send("Hello World!"));
+
+connect()
+  .then(db => {
+    console.log("connected to mongodb");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  })
+  .catch(err => {
+    console.error("mongodb connection failed", err);
+  });
